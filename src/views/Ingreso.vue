d451d1f8fe44e4c38ede155787d2cbca267372f2
Javier F <arcadesoul1@gmail.com>
2020-10-27 06:55:10 +0100

    Add Ingreso

--- src/views/Ingreso.vue
+++ src/views/Ingreso.vue
@@ -13,6 +13,61 @@
                 label="Búsqueda" single-line hide-details></v-text-field>
                 <v-spacer></v-spacer>
                 <v-btn color="primary" v-if="nuevoIngreso==0" @click="mostrarNuevo()" dark class="mb-2">Nuevo</v-btn>
+                <v-dialog v-model="modalArticulos" max-width="1000px">
+                    <v-card>
+                        <v-card-title> 
+                            <span class="headline">Seleccione un artículo</span>
+                        </v-card-title>            
+                        <v-card-text>
+                            <v-container grid-list-md>
+                                <v-layout wrap>
+                                    <v-flex xs12 sm12 md12 lg12 xl12>
+                                        <v-text-field v-model="textoBusqueda" @keyup.enter="getArticulos()" class="text-xs-center" append-icon="search" 
+                                        label="Búsqueda"></v-text-field>
+                                        <template>
+                                            <v-data-table
+                                                :headers="cabeceraArticulos"
+                                                :items="articulos"
+                                                class="elevation-1"
+                                            >
+                                                <template v-slot:item="props">
+                                                    <tr>
+                                                        <td class="justify-center layout px-0">
+                                                            <v-icon
+                                                            class="mr-2"
+                                                            @click="agregarDetalle(props.item)"
+                                                            >
+                                                            add
+                                                            </v-icon>                                                    
+                                                        </td>
+                                                        <td>{{ props.item.codigo }}</td>
+                                                        <td>{{ props.item.nombre }}</td>
+                                                        <td>{{ props.item.categoria.nombre }}</td>
+                                                        <td>{{ props.item.stock }}</td>
+                                                        <td>{{ props.item.precio_venta }}</td>
+                                                        <td>{{ props.item.descripcion }}</td>
+                                                        <td>
+                                                            <div v-if="props.item.estado">
+                                                                <span class="blue--text">Activo</span>
+                                                            </div>
+                                                            <div v-else>
+                                                                <span class="red--text">Inactivo</span>
+                                                            </div>
+                                                        </td> 
+                                                    </tr>               
+                                                </template>
+                                            </v-data-table>
+                                        </template>
+                                    </v-flex>
+                                </v-layout>
+                            </v-container>
+                        </v-card-text>
+                        <v-card-actions>
+                        <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" @click="close">Cancelar</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
                 <v-dialog v-model="adModal" max-width="290">
                     <v-card>
                         <v-card-title class="headline" v-if="adAction==1">
@@ -100,7 +155,7 @@
                 <v-layout row wrap>
                     <v-flex xs12 sm4 md4 lg4 xl4>
                         <v-select v-model="editedItem.tipo_comprobante" 
-                        :items="comprobantes" label="Tipo Comprobante">
+                        :items="comprobantes" label="Tipo Comprobante" >
                         </v-select>
                     </v-flex>
                     <v-flex xs12 sm4 md4 lg4 xl4>
@@ -124,7 +179,7 @@
                         </v-text-field>
                     </v-flex>
                     <v-flex xs12 sm2 md2 lg2 xl2>
-                        <v-btn small fab dark color="teal" >
+                        <v-btn small fab dark color="teal" @click="mostrarModalArticulos">
                             <v-icon dark>list</v-icon>
                         </v-btn>
                     </v-flex>
@@ -158,14 +213,14 @@
                             </v-data-table>
                             <v-flex class="text-xs-right">
                                 <strong>Total sin impuestos:</strong> 
-                                {{ totalNeto = (total-totalIVA).toFixed(2) }} €
+                                {{ totalNeto = (editedItem.total-totalIVA).toFixed(2) }} €
                             </v-flex>
                             <v-flex class="text-xs-right">
                                 <strong>Total impuesto:</strong>
-                                {{ totalIVA = (((total)/(1+editedItem.impuesto))*(editedItem.impuesto)).toFixed(2) }} €
+                                {{ totalIVA = (((editedItem.total)/(1+editedItem.impuesto))*(editedItem.impuesto)).toFixed(2) }} €
                             </v-flex>
                             <v-flex class="text-xs-right">
-                                <strong>Total:</strong> {{ total = calcularTotal }} €
+                                <strong>Total:</strong> {{ editedItem.total = calcularTotal }} €
                             </v-flex>
                         </template>
                     </v-flex>
@@ -176,7 +231,7 @@
                     </v-flex>  
                     <v-flex xs12 sm12 md12 lg12 xl12 d-flex mt-4 style="justify-content: space-evenly;">
                         <v-btn color="blue darken-1" @click="cierraNuevoIngreso">Cancelar</v-btn>
-                        <v-btn color="success" >Guardar</v-btn>
+                        <v-btn color="success" @click="guardarIngreso" >Guardar</v-btn>
                     </v-flex>
                 </v-layout>
             </v-container>
@@ -188,32 +243,8 @@ import axios from 'axios';
     export default {
         data(){
             return{
-                nombreRule: v  => {
-                    if ( v == null || (v && v.length >= 1 && v.length <= 50) ) return true;
-                    return 'Debe tener entre 1-50 caracteres';
-                },
-                rolRule: v  => {
-                    if ( v == null || (v && v.length >= 1 && v.length <= 30) ) return true;
-                    return 'Debe tener entre 1-50 caracteres';
-                },
-                docRule: v  => {
-                    if ( v == null || v.length <= 20 ) return true;
-                    return 'Debe tener máximo 20 caracteres';
-                },
-                direccionRule: v  => {
-                    if ( v == null || v.length <= 70 ) return true;
-                    return 'Debe tener máximo 70 caracteres';
-                },
-                emailRule: v  => {
-                    if (v == null || (v && v.length >= 1 && v.length <= 50) ) return true;
-                    return 'Es requerido (MAX: 50 caracteres)';
-                },
-                passwordRule: v  => {
-                    if ( v == null || (v && v.length >= 1 && v.length <= 64) ) return true;
-                    return 'Es requerido (MAX: 64 caracteres)';
-                },
+                textoBusqueda: '',
                 nuevoIngreso: false,
-                dialog: false,
                 search:'',
                 headers: [
                     { text: 'Opciones', value: 'opciones', sortable: false },
@@ -234,7 +265,16 @@ import axios from 'axios';
                     { text: 'Precio', value: 'precio', sortable: false },
                     { text: 'Sub Total', value: 'subtotal', sortable: false  }
                 ],
-                editedIndex: -1,
+                cabeceraArticulos: [
+                    { text: 'Seleccionar', value: 'seleccionar', sortable: false },
+                    { text: 'Código',value: 'codigo', sortable: false},
+                    { text: 'Nombre',value: 'nombre', sortable: true},
+                    { text: 'Categoría',value: 'categoria.nombre', sortable: true},
+                    { text: 'Stock',value: 'stock', sortable: false},
+                    { text: 'Precio Venta',value: 'precio_venta', sortable: false},
+                    { text: 'Descripción', value: 'descripcion', sortable: false },              
+                    { text: 'Estado', value: 'estado', sortable: false }
+                ],
                 editedItem:
                     {
                         _id: '',
@@ -253,23 +293,19 @@ import axios from 'axios';
                 detalles: [], 
                 articulos: [],
                 codigo: '',
-                total: 0,
                 totalIVA:0,
                 totalNeto:0,
                 errorArticulo:null,
+                modalArticulos: false,
                 valida:0,
-                validaMensaje:[],
+                validaMensaje:[],  
                 adModal:0,
                 adAction:0,
                 adNombre:'',
-                adId:'',
-                
+                adId:''              
             }
         },
         computed: {
-            formTitle () {
-                return this.editedIndex === -1 ? 'Nuevo Usuario' : 'Editar Usuario'
-            },
             calcularTotal(){
                 let suma=0;
                 let total=0;
@@ -287,7 +323,6 @@ import axios from 'axios';
         },
         created () {
             this.getArrayPersonas();
-            this.getArrayArticulos()
             this.listar();
         },
         methods: {
@@ -308,8 +343,7 @@ import axios from 'axios';
                 this.errorArticulo=null;
                 if (this.encuentra(data._id)==true){
                     this.errorArticulo='El artículo ya ha sido agregado.';
-                }
-                else{
+                }else{
                     this.detalles.push(
                         {
                             '_id': data._id,
@@ -336,16 +370,16 @@ import axios from 'axios';
             mostrarNuevo(){
                 this.nuevoIngreso = true;
             },
-            getArrayArticulos(){
+            mostrarModalArticulos(){
+                this.modalArticulos=true;
+            },
+            getArticulos(){
                 let me = this;
-                let articulosArray=[];
                 let header = { "Token" : this.$store.state.token};
                 let configuracion = {headers: header};
-                axios.get('articulo/list',configuracion).then(function (response){
-                    articulosArray=response.data;
-                    articulosArray.map(function(x){
-                        me.articulos.push({articulo:x.nombre, cantidad: x.stock, precio: x.precio_venta});
-                    });
+                axios.get('articulo/list?valor='+this.textoBusqueda,configuracion).then(function (response){
+                    //console.log(me.articulos);
+                    me.articulos=response.data;
                 }).catch(function(error){
                     console.log(error);
                 });
@@ -358,7 +392,7 @@ import axios from 'axios';
                 axios.get('persona/listProveedores',configuracion).then(function (response){
                     proveedoresArray=response.data;
                     proveedoresArray.map(function(x){
-                        me.personas.push({text:x.nombre});
+                        me.personas.push({text:x.nombre, value: x._id});
                     });
                 }).catch(function(error){
                     console.log(error);
@@ -376,94 +410,73 @@ import axios from 'axios';
                 });
             },
             limpiar(){
-                this.editedItem = [{ 
-                    _id:'', 
-                    nombre:'',
-                    rol:'',
-                    num_documento:'',
+                this.editedItem = { 
+                    _id: '',
+                    persona: '',
+                    tipo_comprobante: '',
+                    serie_comprobante: '',
                     tipo_documento: '',
-                    direccion:'',
-                    telefono:'',
-                    email:'',
-                    password:'',
-                }];
+                    num_comprobante: '',
+                    impuesto: 0.21,
+                    total: 0,
+                };
+                this.detalles=[];
+                this.totalIVA=0,
+                this.totalNeto=0,
                 this.valida=0;
                 this.validaMensaje=[];
-                this.editedIndex=-1;
             },
             validar(){
                 this.valida=0;
                 this.validaMensaje=[];
-
+                if(!this.editedItem.persona){
+                    this.validaMensaje.push('Seleccione un proveedor.');
+                }
+                if(!this.editedItem.tipo_comprobante){
+                    this.validaMensaje.push('Seleccione un tipo de comprobante.');
+                }
+                if(!this.editedItem.num_comprobante){
+                    this.validaMensaje.push('Ingrese el número del comprobante.');
+                }
+                if(!this.editedItem.impuesto || this.editedItem.impuesto<0 || this.editedItem.impuesto>1){
+                    this.validaMensaje.push('Ingrese un impuesto válido.');
+                }
+                if(this.detalles.length<=0){
+                    this.validaMensaje.push('Ingrese al menos un artículo al detalle');
+                }
                 if (this.validaMensaje.length){
                     this.valida=1;
                 }
                 return this.valida;
             },
-            guardar(){
+            guardarIngreso(){
                 let me = this;
                 let header = { "Token" : this.$store.state.token};
                 let configuracion = {headers: header};
                 if (this.validar()){
                     return;
                 }
-                if (this.editedIndex > -1) {
-                    // Edita Usuario
-                    axios.put('usuario/update',
+                axios.post('ingreso/add', 
                         {
-                            '_id':this.editedItem._id,
-                            'rol':this.editedItem.rol,
-                            'nombre':this.editedItem.nombre,
-                            'tipo_documento':this.editedItem.tipo_documento,
-                            'num_documento':this.editedItem.num_documento,
-                            'direccion':this.editedItem.direccion,
-                            'telefono': this.editedItem.telefono,
-                            'email':this.editedItem.email,
-                            'password':this.editedItem.password
-                        },
-                        configuracion)
-                        .then( response => {
-                            //console.log(response.data);
-                            //me.editedItem = Object.assign({}, response.data );
-                            me.limpiar();
-                            me.close();
-                            me.listar();
-                            return response;
-                        })
-                        .catch(function(error){
-                            console.log(error);
-                        });
-                } else {
-                    // Guarda nueva Usuario
-                    console.log(this.editedItem);
-                    axios.post('usuario/add', 
-                            {
-                                '_id':this.editedItem._id,
-                                'rol':this.editedItem.rol,
-                                'nombre':this.editedItem.nombre,
-                                'tipo_documento':this.editedItem.tipo_documento,
-                                'num_documento':this.editedItem.num_documento,
-                                'direccion':this.editedItem.direccion,
-                                'telefono': this.editedItem.telefono,
-                                'email':this.editedItem.email,
-                                'password':this.editedItem.password
-                            }
-                            ,configuracion)
-                        .then( response => {
-                            me.limpiar();
-                            me.close();
-                            me.listar();
-                            return response;
-                        }).catch(function(error){
-                            console.log(error);
-                        });
-                }
-            },
-            editItem (item) {
-                this.editedIndex = this.usuarios.indexOf(item)
-                this.editedItem = Object.assign({}, item)
-                console.log(this.editedItem)
-                this.dialog = true
+                            'persona':this.editedItem.persona,
+                            'usuario':this.$store.state.usuario._id,
+                            'tipo_comprobante': this.editedItem.tipo_comprobante,
+                            'serie_comprobante': this.editedItem.serie_comprobante,
+                            'num_comprobante':this.editedItem.num_comprobante,
+                            'impuesto': this.editedItem.impuesto,
+                            'total':this.editedItem.total,
+                            'detalles':this.detalles
+                        }
+                        ,configuracion)
+                    .then( response => {
+                        console.log(response.data)
+                        me.limpiar();
+                        me.cierraNuevoIngreso();
+                        me.listar();
+                        return response;
+                    }).catch(function(error){
+                        console.log(error);
+                    });
             },
             activarDesactivarMostrar(cod, item){
                 this.adAction = cod ; 
@@ -510,9 +523,8 @@ import axios from 'axios';
             cerrarModalActDes(){
                 this.adModal = 0 ;
             },
-            close () {
-                this.limpiar();
-                this.dialog = false
+            close() {
+                this.modalArticulos = false;
             },
             cierraNuevoIngreso(){
                 this.nuevoIngreso = false;
